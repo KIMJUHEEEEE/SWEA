48ef36e85f8aa22c7123eb7d35fb6a4385648659
KIMJUHEEEEE <jhwngml97@gmail.com>
2019-09-09 14:42:39 +0900

    SWEA #8104

---
+++ 8104.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int tc = 0;
+	cin >> tc;
+	for (int i = 1; i <= tc; i++)
+	{
+		int arr[21] = { 0, };
+		int N, K = 0;
+		cin >> N >> K;
+		for (int k = 1; k <= N; k++)
+		{
+			if (k % 2 == 1)
+			{
+				for (int i = 1; i <= K; i++)
+				{
+					arr[i] += (k-1)*K + i;
+				}
+			}
+			else
+			{
+				for (int i = 1; i <= K; i++)
+				{
+					arr[i] += k * K + 1 - i;
+				}
+			}
+		}
+		cout << '#' << i << ' ';
+		for (int i = 1; i <= K; i++)
+		{
+			cout << arr[i] << ' ';
+		}
+		cout << endl;
+	}
+}
