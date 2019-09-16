f82f3086e3255b5fc22dda46be7aeb76eb49110b
KIMJUHEEEEE <jhwngml97@gmail.com>
2019-09-17 00:12:25 +0900

    알 덴테 스파게티

---
+++ 8457.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t = 0;
+	cin >> t;
+	for (int i = 1; i <= t; i++)
+	{
+		int n, b, e;
+		cin >> n>>b>>e;
+		int min = b - e;
+		int max = b + e;
+		int arr[101] = { 0, };
+		for (int i = 0; i < n; i++)
+		{
+			cin >> arr[i];
+		}
+		int answer = 0;
+		for (int i = 0; i < n; i++)
+		{int a = 1;
+		while (max >= a *arr[i])
+		{
+			if (arr[i] * a >= min && arr[i] * a <= max)
+			{
+				answer++;
+				break;
+			}
+			else
+			{
+				a++;
+			}
+		}
+
+		}
+		cout << '#' << t << ' ' << answer<<endl;
+	}
+}
