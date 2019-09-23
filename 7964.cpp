3a687be5b1da135c01515b567762316ef6f93777
KIMJUHEEEEE <jhwngml97@gmail.com>
2019-09-23 20:30:36 +0900

    부먹왕국의 차원 관문

---
+++ 7964.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<algorithm>
+#include <vector>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int t = 0;
+	cin >> t;
+	for (int i = 1; i <= t; i++)
+	{
+		int n, d;
+		vector<int> v;
+		cin >> n >> d;
+		for (int i = 0; i < n; i++)
+		{
+			int a;
+			cin >> a;
+			v.push_back(a);
+		}
+		int num = 0;
+		int ans = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (v[i] == 0)
+			{
+				num++;
+			}
+			else
+				num = 0;
+			if (num == d)
+			{
+				ans++;
+				num = 0;
+			}
+		}
+		cout << '#' << i << ' ' << ans<<endl;
+	}
+}
