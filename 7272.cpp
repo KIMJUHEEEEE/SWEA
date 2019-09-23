3513de59b443adc31d8eb82aee56f099f952fec1
KIMJUHEEEEE <jhwngml97@gmail.com>
2019-09-23 20:47:43 +0900

    !

--- 7272.cpp
+++ 7272.cpp
@@ -2,43 +2,44 @@
 #include <string>
 using namespace std;
 
-int sol(char a)
-{
-	if (a == 'A' || a == 'D' || a == 'O' || a == 'P' || a == 'Q' || a == 'R')
-		return 1;
-	else if (a == 'B')
-		return 2;
-	else
-		return 0;
-}
 int main() {
-	int n = 0;
-	scanf("%d", &n);
-	for (int i = 1; i <= n; i++)
+	int T = 0;
+	cin >> T;
+	for (int i = 1; i <= T; i++)
 	{
-		string a, b;
-		cin >> a >> b;
-		printf("#%d ", i);
-		if (a.size() != b.size())
+		int n = 0;
+		cin >> n;
+		string a;
+		getline(cin, a);
+		for (int i = 0; i < a.size(); i++)
 		{
-			printf("DIFF\n");
-			continue;
-		}
-		else
-		{
-			int ss = 0;
-			for (int j = 0; j < a.size(); j++)
-			{
-				if (sol(a[j]) != sol(b[j]))
+			int num = 0;
+			int ans = 0;
+			if (a[i]>='A'&&a[i]<='Z')
 				{
-					printf("DIFF\n");
-					break;
+				if (i == 0 || a[i - 1] == ' ')
+				{
+					num++;
+					while (a[i] != ' ' && a[i] != '!' && a[i] != '.' && a[i] != '?')
+					{
+						i++;
+						if (a[i] >= 'a' && a[i] <= z)
+						{
+							num++;
+						}
+						else
+						{
+							ans = 0;
+							break;
+						}
+					}
+					if (num == a.size())
+				}		ans++;
 				}
-				else
-					ss++;
 			}
-			if (ss == a.size())
-				printf("SAME\n");
+		cout << ans << ' ';
+		ans = 0;
 		}
+	cout << endl;
 	}
 }
