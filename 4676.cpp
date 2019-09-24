1ba7dd719724f4f06d6f94f2fe28b60eb0d336c4
KIMJUHEEEEE <jhwngml97@gmail.com>
2019-09-24 22:23:44 +0900

    늘어지는 소리 만들기

---
+++ 4676.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<algorithm>
+#include <vector>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int t = 0;
+	cin >> t;
+	for (int i = 1; i <= t; i++)
+	{
+		string a;
+		cin >> a;
+		int h = 0;
+		cin >> h;
+		int arr[101] = { 0, };
+		int k = 0;
+		for (int i = 0; i < h; i++)
+		{
+			cin >> k;
+			arr[k]++;
+		}
+		string ans = "";
+		for (int i = 0; i <= a.size(); i++)
+		{
+
+			if (arr[i] != 0)
+			{
+				while (1)
+				{
+					ans += '-';
+					arr[i]--;
+					if (arr[i] == 0)
+						break;
+				}
+			}
+			if(i<a.size())
+				ans += a[i];
+		}
+		
+		cout << "#" << i << ' ' << ans << endl;
+
+	}
+}
