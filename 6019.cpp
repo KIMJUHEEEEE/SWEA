1f1365557d2802ff114aa21e36d87f3f6a3707eb
KIMJUHEEEEE <jhwngml97@gmail.com>
2019-09-30 14:11:26 +0900

    기차 사이의 파리

---
+++ 6019.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<vector>
+using namespace std;
+
+
+int main() {
+	int t = 0;
+	cin >> t;
+	for (int i = 1; i <= t; i++)
+	{
+		float d, a, b, f;
+		cin >> d >> a >> b >> f;
+		cout<<fixed;
+		cout.precision(7);
+		float ans = d / (a + b) * f;
+		cout << '#' << i << ' ' << ans<<endl;
+	}
+}
