7bd37f451172915c3ffbd957f860c2802829e541
KIMJUHEEEEE <jhwngml97@gmail.com>
2019-12-26 17:00:25 +0900

    하지 추측

---
+++ 8993.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+int main() {
+	int tc = 0;
+	cin >> tc;
+	for (int i = 1; i <= tc; i++)
+	{
+		long long n = 0;
+		cin >> n;
+		int k = n;
+		cout << '#' << i << ' ';
+		while (n > 1)
+		{
+			if (n % 2 == 0)
+				n = n / 2;
+			else
+				break;
+		}
+		if (n == 1)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+	}
+}
