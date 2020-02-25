f0031397b35daf6039ac267697decdb9756b3e76
KIMJUHEEEEE <jhwngml97@naver.com>
2020-02-25 16:50:22 +0900

    사칙연산 유효성 검사

---
+++ 1233.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+
+int main() {
+	for (int i = 1; i <= 10; i++)
+	{
+		int n = 0;
+		cin >> n;
+		bool ans = 0;
+		int node[204] = { 0, };
+		int q = 1;
+		for (int i = 1; i <= n; i++)
+		{
+			char op;
+			scanf("%*d %c", &op);
+			if ((n % 2 == 0 && (i < n / 2)) || (n % 2 == 1 && i <= (n - 1) / 2))
+				scanf(" %*d %*d");
+			else if (n % 2 == 0 && i == n / 2)
+				scanf(" %*d");
+			node[i] = op;
+		}
+		cout << '#' << i << ' ';
+		for (int i = 1; i < n; i++)
+		{
+			if (i < n / 2)
+			{
+				if (node[i] != '+' && node[i] != '-' && node[i] != '/' && node[i] != '*')
+				{
+					ans = 1;
+					cout << ans << endl;
+					break;
+				}
+			}
+			else {
+				if (node[i] < '0' || node[i] > '9')
+				{
+					ans = 1;
+					cout << ans << endl;
+					break;
+				}
+			}
+
+		}
+	}
+	return 0;
+}
